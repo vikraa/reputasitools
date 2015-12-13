03b704d1777934bf87b8ab6ebb2673291e896a5a
vikra.alviano@gmail.com <vikra.alviano@gmail.com>
2015-12-13 23:46:26 +0700

    add new receiver skeleton

---
+++ app/src/main/java/com/labs/tools/receiver/SmsReceiver.java
@@ -0,0 +1,15 @@
+package com.labs.tools.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by vikraa on 12/13/2015.
+ */
+public class SmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
